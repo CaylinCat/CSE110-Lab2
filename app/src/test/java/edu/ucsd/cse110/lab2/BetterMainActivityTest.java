d51e68b62f6643fdf0c17c16a20eb928bb2f468b
CaylinCat <caylin.email@gmail.com>
2023-01-17 16:27:39 -0800

    finished lab

---
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActivityTest.java
@@ -0,0 +1,48 @@
+package edu.ucsd.cse110.lab2;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+
+@RunWith(RobolectricTestRunner.class)
+public class BetterMainActivityTest {
+    @Test
+    public void test_one_plus_one_equals_two() {
+//        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+//        scenario.moveToState(Lifecycle.State.CREATED);
+//        scenario.moveToState(Lifecycle.State.STARTED);
+//
+//        scenario.onActivity(activity -> {
+//            activity.findViewById(R.id.btn_one).performClick();
+//            activity.findViewById(R.id.btn_plus).performClick();
+//            activity.findViewById(R.id.btn_one).performClick();
+//            activity.findViewById(R.id.btn_equals).performClick();
+//            assertEquals((TextView) activity.findViewById(R.id.display).getTe, "2");
+//            assertEquals(activity.getText(R.id.display)), "2");
+//        });
+
+        try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+            controller.setup();
+            controller.pause();
+            controller.stop();
+            MainActivity activity = controller.get();
+            activity.findViewById(R.id.btn_one).performClick();
+            activity.findViewById(R.id.btn_plus).performClick();
+            activity.findViewById(R.id.btn_one).performClick();
+            activity.findViewById(R.id.btn_equals).performClick();
+
+            assertEquals(((TextView) activity.findViewById(R.id.display)).getText(), "2");
+        }
+    }
+}
